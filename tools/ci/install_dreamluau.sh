6e4e6b029dc9424c9c6b883c768833cac7348522
FeudeyTF <yanmordanenko089@gmail.com>
2025-08-16 14:11:15 +0300

    s

--- tools/ci/install_dreamluau.sh
+++ tools/ci/install_dreamluau.sh
@@ -6,4 +6,4 @@ source dependencies.sh
 mkdir -p ~/.byond/bin
 wget -nv -O ~/.byond/bin/libdreamluau.so "https://github.com/$DREAMLUAU_REPO/releases/download/$DREAMLUAU_VERSION/libdreamluau.so"
 chmod +x ~/.byond/bin/libdreamluau.so
-ldd ~/.byond/bin/libdreamluau.s
+ldd ~/.byond/bin/libdreamluau.so
